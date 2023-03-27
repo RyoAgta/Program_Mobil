71cae78f98fa491943362bd21a6a1b10c7756f0a
RyoZAgta <ryoagta01@gmail.com>
2023-03-27 21:43:26 +0700

    Mobil

---
+++ Latihan2ClassdanObject/Mobil.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Latihan2ClassdanObject
+{
+    internal class Mobil
+    {
+        public string warna;
+        public int JumlahPintu;
+        public string merek;
+        public string model;
+        public int TahunKeluar;
+
+
+        public void Gas(int kecepatan)
+        {
+            Console.WriteLine("Mobil {0} berjalan dengan kecepatan {1}", model, kecepatan);
+        }
+
+        public void Klakson(string suara)
+        {
+            Console.WriteLine("{0}", suara);
+        }
+
+        public void TampilkanInfo()
+        {
+            Console.WriteLine("Mobil saya berwarna {0}, merek {1} model {2} keluaran tahun {3} dengan jumlah pintu sebanyak {4}", warna, merek, model, TahunKeluar, JumlahPintu);
+        }
+
+    }
+}
---
+++ Latihan2ClassdanObject/Program.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Latihan2ClassdanObject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Mobil mobil1 = new Mobil();
+            mobil1.warna = "Red";
+            mobil1.JumlahPintu = 2;
+            mobil1.merek = "Ferrari";
+            mobil1.model = "mobil sport";
+            mobil1.TahunKeluar = 2010;
+
+            mobil1.Gas(300);
+            mobil1.Klakson("Beemm Bemm");
+            mobil1.TampilkanInfo();
+
+            Console.WriteLine("\n");
+            Console.WriteLine("===================");
+
+            Mobil mobil2 = new Mobil();
+            mobil2.warna = "Yellow";
+            mobil2.JumlahPintu = 2;
+            mobil2.merek = "mustang GT";
+            mobil2.model = "mobil sport";
+            mobil2.TahunKeluar = 2012;
+
+            mobil2.Gas(200);
+            mobil2.Klakson("Tenn Tenn");
+            mobil2.TampilkanInfo();
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
